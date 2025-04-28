2fdc2cbabc26a29a9d7c3980c4a5bad1bc6d1a32
Vandan Rohatgi <vandan@Vandans-MacBook-Air.local>
2025-04-28 17:17:32 +0530

    handle multiple pings

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
@@ -17,14 +18,24 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		var dataFromConn []byte
+		_, err = conn.Read(dataFromConn)
+		if err != nil {
+			log.Fatal("unable to read")
+		}
+		fmt.Println(dataFromConn)
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error writing response: ", err)
+			os.Exit(1)
+		}
 	}
-	_, err = conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Error writing response: ", err)
-		os.Exit(1)
-	}
+
 }
